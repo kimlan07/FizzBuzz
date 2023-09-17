0b04f6a743c8c2a9a5dd932a189695450341102d
kimlan <kimlan.huynh@mail.utoronto.ca>
2023-09-17 17:06:50 -0400

    New Java Class to reduce number to 0

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+
+        int steps = 0;
+        int n = 100;
+        while (n > 0) {
+
+            boolean even = n % 2 == 0;
+            boolean odd = n % 2 == 1;
+
+            if (even) {
+                n = n / 2;
+                steps++;
+
+            } else if (odd) {
+                n = n - 1;
+                steps++;
+            }
+        } System.out.println(steps);
+    }
 }
